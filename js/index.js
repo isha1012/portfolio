578b899bde2189caf95efd3d93a1b732eb751553
unknown <aishafehintola@gmail.com>
2020-02-24 09:34:03 +0100

    files

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// var full = setInterval(time,2000); {
+//     function time(){
+//             document.getElementById("display").innerHTML="Full-stack Developer";     
+//     }
+   
+// }
+// function time(){
+    // document.getElementById("display").innerHTML="Full-stack Developer";
+// }
